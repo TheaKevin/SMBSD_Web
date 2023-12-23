67d84c1b126a66bbf456172f6235673114e96fb4
TheaKevin <theakevin01@gmail.com>
2023-12-23 20:49:29 +0700

    ubah title web jd SMB Suvanna Dipa

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title style="">Curhatku</title>
+        <title style="">SMB Suvanna Dipa</title>
 
         <!-- Scripts -->
 
